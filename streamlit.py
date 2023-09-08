be1fe43280f15f9e576ec75ed8c7e89e52729abd
hadiana Sliwa <hadiana.eng@gmail.com>
2023-09-08 17:35:45 +0300

    new

---
+++ streamlit.py
@@ -0,0 +1,11 @@
+import xgboost as xgb
+import streamlit as st
+import pandas as pd
+
+#Loading up the Regression model we created
+model = xgb.XGBRegressor()
+model.load_model('xgb_model.json')
+
+#Caching the model for faster loading
+@st.cache
+
