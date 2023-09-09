d140bb55cf792ce036113f1d70e2aef472a7016f
hadiana Sliwa <hadiana.eng@gmail.com>
2023-09-09 22:33:57 +0300

    adding update function to streamlit

--- streamlit.py
+++ streamlit.py
@@ -74,6 +74,8 @@ x = st.number_input('Diamond Length (X) in mm:', min_value=0.1, max_value=100.0,
 y = st.number_input('Diamond Width (Y) in mm:', min_value=0.1, max_value=100.0, value=1.0)
 z = st.number_input('Diamond Height (Z) in mm:', min_value=0.1, max_value=100.0, value=1.0)
 
+def update():
+    print('updating...')
 
 if st.button('Predict Price'):
     price = predict(carat, cut, color, clarity, depth, table, x, y, z)
